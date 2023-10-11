63736bcebb33d2e42c89875079a06260ac681776
EverLand1 <126717167+EverLand1@users.noreply.github.com>
2023-10-11 18:11:47 -0500

    QuicKitty Script

---
+++ QuicKitty.ps1
@@ -0,0 +1,54 @@
+####################################
+##### Runs powershell as admin #####
+####################################
+
+Start-process powershell -Verb runAs
+
+###########################################
+##### Enable running unsigned scripts #####
+###########################################
+
+set-executionpolicy remotesigned
+
+##################################
+##### SSL/TLS Secure Channel #####
+##################################
+
+# Powershell by default uses TLS 1.0 to connect to website, but website security requires TLS 1.2
+
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+
+######################################################
+##### Download Lastest Release of HardeningKitty #####
+######################################################
+
+Function InstallHardeningKitty() {
+    $Version = ((Invoke-WebRequest "https://api.github.com/repos/0x6d69636b/windows_hardening/releases/latest" -UseBasicParsing) | ConvertFrom-Json).Name
+    $HardeningKittyLatestVersionDownloadLink = ((Invoke-WebRequest "https://api.github.com/repos/0x6d69636b/windows_hardening/releases/latest" -UseBasicParsing) | ConvertFrom-Json).zipball_url
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest $HardeningKittyLatestVersionDownloadLink -Out HardeningKitty$Version.zip
+    Expand-Archive -Path ".\HardeningKitty$Version.zip" -Destination ".\HardeningKitty$Version" -Force
+    $Folder = Get-ChildItem .\HardeningKitty$Version | Select-Object Name -ExpandProperty Name
+    Move-Item ".\HardeningKitty$Version\$Folder\*" ".\HardeningKitty$Version\"
+    Remove-Item ".\HardeningKitty$Version\$Folder\"
+    New-Item -Path $Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version -ItemType Directory
+    Set-Location .\HardeningKitty$Version
+    Copy-Item -Path .\HardeningKitty.psd1,.\HardeningKitty.psm1,.\lists\ -Destination $Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version\ -Recurse
+    Import-Module "$Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version\HardeningKitty.psm1"
+}
+
+#####################################################
+##### Installs HardeningKitty Powershell Module #####
+#####################################################
+
+InstallHardeningKitty
+
+echo Ka-Chow!!!!!!!!!!!!!!
+
+#################################################
+##### Hardening Kitty Hail Marys the System #####
+#################################################
+
+Invoke-HardeningKitty -Mode HailMary -Log -Report -FileFindingList .\lists\finding_list_0x6d69636b_machine.csv
+
+echo You Did It!!!!!!!!!! You won Hivestorm!!!!!!!!!!
